e1bb1253a4b762e55712df7b156778db3744f4e1
Joa <joaquin@pixelada.org>
2019-05-06 13:11:50 +0200

    FIX: PEP8 compliance and starting to make tests for setting primary display of game using SDL

--- src/start.py
+++ src/start.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
-
 #
 #  Freedom Fighters of Might & Magic
 #
-#  Copyright 2014-2015 by it's authors. 
+#  Copyright 2014-2015 by it's authors.
 #
 #  Some rights reserved. See COPYING, AUTHORS.
 #  This file may be used under the terms of the GNU General Public
@@ -15,70 +14,70 @@
 #  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #
 
-
-
 try:
-   import sys 
-   import random
-   import math
-   import getopt
-   import pygame
-   import main_menu
-   import game_engine
-   import utils
-   import sounds
-   import paths
-   import gummworld2
-   from gummworld2 import Engine, State, BasicMap, SubPixelSurface, View, Vec2d
-   from gummworld2 import context, model, spatialhash, toolkit, data
-
-   from socket import *
-   from pygame.locals import *
+    import os
+    import sys
+    import random
+    import math
+    import getopt
+    import pygame
+    import main_menu
+    import game_engine
+    import utils
+    import sounds
+    import paths
+    import gummworld2
+    from gummworld2 import Engine, State, BasicMap, SubPixelSurface, View, Vec2d
+    from gummworld2 import context, model, spatialhash, toolkit, data
+    from socket import *
+    from pygame.locals import *
 except ImportError as err:
-   print("couldn't load module. %s" % (err))
-   sys.exit(2)
+    print("couldn't load module. %s" % (err))
+    sys.exit(2)
+
 
 class app:
+    def __init__(self, parameters):
+        # sound
+        self.gameSounds = sounds.gameSound()
+        self.gameSounds.loadTracks()
+        pygame.init()
+        if(parameters['resolution'] == 'NULL'):
+            parameters['resolution'] = (1024, 768)
+        if(parameters['strcaption'] == 'NULL'):
+            parameters['strcaption'] = 'Freedom Fighters of Might & Magic'
+        self.parameters = parameters
 
- def __init__(self,parameters):
-    #sound
-    self.gameSounds = sounds.gameSound()
-    self.gameSounds.loadTracks()
-    pygame.init();
-    if(parameters['resolution'] == 'NULL'):
-       parameters['resolution'] = (1024,768)
-    if(parameters['strcaption'] == 'NULL'):
-       parameters['strcaption'] = 'Freedom Fighters of Might & Magic'
-    self.parameters = parameters
+    def setDisplay(self):
+        os.environ['SDL_VIDEO_FULLSCREEN_HEAD'] = '2'
+        # self.screen =
+        #     pygame.display.set_mode(self.resolution,pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode(self.parameters['resolution'])
+        self.caption = pygame.display.set_caption(
+                self.parameters['strcaption'])
+        self.parameters['caption'] = self.caption
 
-
- def setDisplay(self):
-    #self.screen =  pygame.display.set_mode(self.resolution,pygame.FULLSCREEN)
-    self.screen =  pygame.display.set_mode(self.parameters['resolution'])
-    self.caption = pygame.display.set_caption(self.parameters['strcaption'])
-    self.parameters['caption'] = self.caption
-
- def run(self):
-    self.gameSounds.playmenu(True,1.0)
-    scr_menu = main_menu.main_menu(self.parameters)
-    scr_menu.setScreen(self.screen,30)
-    scr_menu.constructScene()
-    ret = 0
-    while True:
-       if(ret == 0):
-          ret = scr_menu.run()
-       if(ret == 1):
-          self.gameSounds.playmenu(False,1.0)
-          print("LET'S PLAY FMM!!")  
-          scr_game = game_engine.gameEngine(self.parameters,self.gameSounds)
-          print('in game!!')
-          gummworld2.run(scr_game)
-          print('out of game!!')
-          ret = 0
-          self.gameSounds.playmenu(True,1.0)
-
-       else:
-          self.gameSounds.playmenu(False,1.0)
-          print("EXITING FMM!!")
-          pygame.quit()
-          sys.exit()		
+    def run(self):
+        self.gameSounds.playmenu(True, 1.0)
+        scr_menu = main_menu.main_menu(self.parameters)
+        scr_menu.setScreen(self.screen, 30)
+        scr_menu.constructScene()
+        ret = 0
+        while True:
+            if(ret == 0):
+                ret = scr_menu.run()
+            if(ret == 1):
+                self.gameSounds.playmenu(False, 1.0)
+                print("LET'S PLAY FMM!!")
+                scr_game = game_engine.gameEngine(
+                        self.parameters, self.gameSounds)
+                print('in game!!')
+                gummworld2.run(scr_game)
+                print('out of game!!')
+                ret = 0
+                self.gameSounds.playmenu(True, 1.0)
+            else:
+                self.gameSounds.playmenu(False, 1.0)
+                print("EXITING FMM!!")
+                pygame.quit()
+                sys.exit()
